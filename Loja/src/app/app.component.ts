c4fb20b3900e0b737718601347726bdc2a71b8b8
Matheusdias77 <heyp692@gmail.com>
2024-08-03 16:58:54 -0300

    Finalizando Header

--- Loja/src/app/app.component.ts
+++ Loja/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , HeaderComponent],
+  imports: [RouterOutlet , HeaderComponent , LoginComponent , CadastroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 }
+
