c51eeddc4a15d23bbb6921e0b876836c31dd7565
Matheusdias77 <heyp692@gmail.com>
2024-08-11 01:01:42 -0300

    Header Finalizado

--- Loja/src/app/app.component.ts
+++ Loja/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { ProdutoComponent } from './components/produto/produto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , HeaderComponent , LoginComponent , CadastroComponent , ProdutoComponent],
+  imports: [RouterOutlet , HeaderComponent , LoginComponent , CadastroComponent , ProdutoComponent , FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
