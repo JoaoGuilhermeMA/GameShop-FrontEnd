907d23bfa78ea1f6bfec6bfcab1960bd574c62b2
Matheusdias77 <heyp692@gmail.com>
2024-08-15 18:00:39 -0300

    Finalizando tela de usuario

--- Loja/src/app/app.component.ts
+++ Loja/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { LoginComponent } from './components/login/login.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { ProdutoComponent } from './components/produto/produto.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CarrosselComponent } from './components/carrossel/carrossel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , HeaderComponent , LoginComponent , CadastroComponent , ProdutoComponent , FooterComponent],
+  imports: [RouterOutlet , HeaderComponent , LoginComponent , CadastroComponent , ProdutoComponent , FooterComponent , CarrosselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Loja/src/app/components/carrossel/carrossel.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carrossel',
+  standalone: true,
+  imports: [],
+  templateUrl: './carrossel.component.html',
+  styleUrl: './carrossel.component.css'
+})
+export class CarrosselComponent implements OnInit{
+  ngOnInit(): void {
+    if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+      const images = document.querySelectorAll<HTMLImageElement>('.imagens img');
+      let currentIndex = 0;
+
+      function showNextImage() {
+        images[currentIndex].classList.remove('active');
+        currentIndex = (currentIndex + 1) % images.length;
+        images[currentIndex].classList.add('active');
+      }
+
+      images[currentIndex].classList.add('active');
+      setInterval(showNextImage, 5000);
+    }
+  }
+}
