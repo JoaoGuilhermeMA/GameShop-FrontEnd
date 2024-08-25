3d524830cb378777d8dc5e765dd23b9322039512
Vinicius de Oliveira <viniciusmgomes2005@hotmail.com>
2024-08-25 16:57:39 -0300

    feat: Listando plataformas cadastradas e categorias cadastradas

--- Loja/src/app/components/jogo-form/jogo-form.component.ts
+++ Loja/src/app/components/jogo-form/jogo-form.component.ts
@@ -5,6 +5,8 @@ import {JogoService} from "../../services/jogo.service";
 import {Jogo} from "../../models/jogo";
 import {FormsModule} from "@angular/forms";
 import {Router} from "@angular/router";
+import {CategoriaService} from "../../services/categoria.service";
+import {PlataformaService} from "../../services/plataforma.service";
 
 @Component({
   selector: 'app-jogo-form',
@@ -18,8 +20,33 @@ export class JogoFormComponent {
   @Input("jogo") jogo : Jogo = new Jogo();
   @Output("retorno") retorno = new EventEmitter<any>();
 
+  plataformas: any[] = [];
+  categorias: any[] = [];
+
   jogoService = inject(JogoService);
+  categoriaService = inject(CategoriaService);
+  plataformaService = inject(PlataformaService);
   router = inject(Router);
+
+  ngOnInit() {
+    this.loadPlataformas();
+    this.loadCategorias();
+  }
+
+  loadPlataformas() {
+    this.plataformaService.findAll().subscribe({
+      next: plataformas => this.plataformas = plataformas,
+      error: err => console.error('Erro ao carregar plataformas', err)
+    });
+  }
+
+  loadCategorias() {
+    this.categoriaService.findAll().subscribe({
+      next: categorias => this.categorias = categorias,
+      error: err => console.error('Erro ao carregar categorias', err)
+    });
+  }
+
 
   findById(id: number) {
     this.jogoService.findById(id).subscribe({
