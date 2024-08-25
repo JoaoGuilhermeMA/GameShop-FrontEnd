4299486c3e710e3d711f0d9acd618becce46456b
Matheusdias77 <heyp692@gmail.com>
2024-08-25 18:14:28 -0300

    Finalizando tela de cadastrar jogos

--- Loja/src/app/components/jogo-form/jogo-form.component.ts
+++ Loja/src/app/components/jogo-form/jogo-form.component.ts
@@ -21,7 +21,31 @@ export class JogoFormComponent {
   @Output("retorno") retorno = new EventEmitter<any>();
 
   plataformas: any[] = [];
-  categorias: any[] = [];
+  categorias: { id: number, categoriasJogo: string }[] = [
+    { id: 1, categoriasJogo: 'Ação' },
+    { id: 2, categoriasJogo: 'Aventura' },
+    { id: 3, categoriasJogo: 'RPG' },
+    { id: 4, categoriasJogo: 'Estratégia' },
+    { id: 5, categoriasJogo: 'Simulação' },
+    { id: 6, categoriasJogo: 'Esportes' },
+    { id: 7, categoriasJogo: 'Corrida' },
+    { id: 8, categoriasJogo: 'Luta' },
+    { id: 9, categoriasJogo: 'Sobrevivência' },
+    { id: 10, categoriasJogo: 'Puzzle' },
+    { id: 11, categoriasJogo: 'Realidade Virtual' },
+    { id: 12, categoriasJogo: 'Sandbox' },
+    { id: 13, categoriasJogo: 'Multiplayer' },
+    { id: 14, categoriasJogo: 'Indie' },
+    { id: 15, categoriasJogo: 'Terror' },
+    { id: 16, categoriasJogo: 'Fantasia' },
+    { id: 17, categoriasJogo: 'Sci-Fi' },
+    { id: 18, categoriasJogo: 'Casual' },
+    { id: 19, categoriasJogo: 'Visual Novel' },
+    { id: 20, categoriasJogo: 'Card Game' },
+    { id: 21, categoriasJogo: 'MMO' },
+    { id: 22, categoriasJogo: 'FPS' }
+  ];
+  
 
   jogoService = inject(JogoService);
   categoriaService = inject(CategoriaService);
