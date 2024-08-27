09013a731b7ebed04585cd02c86c6d5d1df37668
JoaoGuilhermeMA <joomoreira36@gmail.com>
2024-08-27 16:04:24 -0300

    listagem

--- Loja/src/app/components/lista-plataforma/lista-plataforma.component.ts
+++ Loja/src/app/components/lista-plataforma/lista-plataforma.component.ts
@@ -1,50 +1,49 @@
-import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {Jogo} from "../../models/jogo";
-import {JogoService} from "../../services/jogo.service";
-import {Plataforma} from "../../models/plataforma";
-import {PlataformaService} from "../../services/plataforma.service";
+import { CommonModule } from '@angular/common';  // Adicionei isso
+import { Plataforma } from "../../models/plataforma";
+import { PlataformaService } from "../../services/plataforma.service";
 
 @Component({
   selector: 'app-lista-plataforma',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],  // Adicionei o CommonModule aqui
   templateUrl: './lista-plataforma.component.html',
-  styleUrl: './lista-plataforma.component.css'
+  styleUrls: ['./lista-plataforma.component.css']  // Corrigido para styleUrls
 })
 export class ListaPlataformaComponent {
 
-  @Input("plataforma") plataforma : Plataforma = new Plataforma();
+  @Input("plataforma") plataforma: Plataforma = new Plataforma();
   @Output("retorno") retorno = new EventEmitter<any>();
 
   plataformas: Plataforma[] = [];
-  plataformaService = inject(PlataformaService);
 
-  constructor() {
+  constructor(private plataformaService: PlataformaService) {  // Injeção via construtor
     this.findAll();
   }
 
-  findAll(){
+  findAll() {
     this.plataformaService.findAll().subscribe({
       next: value => {
         console.log(value);
         this.plataformas = value;
       },
       error: err => {
-        alert("Erro ao mostrar dados: "+ err)
+        console.error("Erro ao mostrar dados: ", err); // Usando console.error em vez de alert
       }
-    })
+    });
   }
 
-  deletePlataforma(idPlataforma: number){
+  deletePlataforma(idPlataforma: number) {
     this.plataformaService.deleteByid(idPlataforma).subscribe({
       next: value => {
-
+        this.plataformas = this.plataformas.filter(plataforma => plataforma.id !== idPlataforma);
+        console.log('Plataforma excluída com sucesso!'); // Usando console.log em vez de alert
       },
       error: err => {
-
+        console.error("Erro ao excluir plataforma: ", err); // Usando console.error em vez de alert
       }
-    })
+    });
   }
 
 }
